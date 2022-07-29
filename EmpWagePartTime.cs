3eb9d019a9811c819d3f98b93018eabdd7bb5731
bhumika1111 <bhoyarpatil@434>
2022-07-29 15:26:08 +0530

    Check Employee Wage Is For Part Time Or Full Time

---
+++ EmpWagePartTime.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWage
+{
+    /// <summary>
+    /// Here We Check Employee is Present For Full Time Or Part Time.
+    /// </summary>
+    public static  class EmpWagePartTime
+    {
+        /// <summary>
+        /// Checks the employee part time.
+        /// </summary>
+        public static void CheckEmployeePartTime()
+        {
+            //constant
+            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
+
+            //variables
+
+            int empwage = 0;
+            int empHrs = 0;
+
+            Random random = new Random();
+
+            int empCheck = random.Next(3);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee Is Full Time");
+                empHrs = 8;
+            }
+            else if (empCheck==IS_PART_TIME)
+            {
+                Console.WriteLine("Employee Is Part Time");
+                empHrs = 4;
+            }
+            else
+            {
+                Console.WriteLine("Employee Is Absent");
+                empHrs = 0;
+            }
+            empwage = EMP_RATE_PER_HOUR * empHrs;
+            Console.WriteLine("Employee Wage:" + empwage);
+
+        }
+    }
+}
