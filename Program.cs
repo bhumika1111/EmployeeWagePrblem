946953485a4eefcd70c3a5f20b914fbb98a1db2f
bhumika1111 <bhoyarpatil@434>
2022-07-29 14:06:23 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(":::::Welcome To EmployeeWage Program::::::");
+            Console.WriteLine("**Output for Employee Is Present Or Absent**");
+            EmployeeCheck.CheckEmployeeAttendance();
+            Console.WriteLine("::::::::::::::::::::::::::::::::::::::::::::");
+            Console.WriteLine("**Output for Employee Daily Wage**");
+            EmpWageDaily.CheckEmpAttendanceFullTime();
+            Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::");
+            Console.WriteLine("**Output For Employee Is Full Time Or Part Time**");
+            EmpWagePartTime.CheckEmployeePartTime();
+            Console.WriteLine("::::::::::::::::::::::::::::::::::::::::::::");
+            Console.WriteLine("**Output For Employee Wage Using Switch Case**");
+            SwitchCase.CheckEmpAttendance();
+            Console.WriteLine("::::::::::::::::::::::::::::::::::::::::::::::");
+            Console.WriteLine("**Output For Employee Wage For Month**");
+            EmpWageMonth.CheckEmpWageForMonth();
+            Console.WriteLine(":::::::::::::::::::::::::::::::::::::::::::::::");
+            Console.WriteLine("**Output For Total Employee Wage In Month**");
+            EmpWageHrsInMonth.CheckEmpWageHrsOrDayInMonth();
+        }
+    }
+}
